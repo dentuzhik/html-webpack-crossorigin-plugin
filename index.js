c86dd519b8176a63ca37a5418eac8087524ee19e
Denis Tuzhik <dentuzhik@gmail.com>
2016-10-07 18:12:34 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+function filterScriptTags(tag) {
+    return tag.tagName === 'script';
+}
+
+function addCrossoriginAttrubuteToScript(script) {
+    script.attributes.crossorigin = 'anonymous';
+}
+
+function addCrossoriginAttrubuteToScripts(htmlPluginData, callback) {
+    htmlPluginData.head.filter(filterScriptTags).forEach(addCrossoriginAttrubuteToScript);
+    htmlPluginData.body.filter(filterScriptTags).forEach(addCrossoriginAttrubuteToScript);
+
+    callback(null);
+}
+
+function HtmlWebpackCrossoriginPlugin() {}
+
+HtmlWebpackCrossoriginPlugin.prototype.apply = function(compiler) {
+    // Hook into the html-webpack-plugin processing
+    compiler.plugin('compilation', function(compilation) {
+        compilation.plugin('html-webpack-plugin-alter-asset-tags', function(htmlPluginData, callback) {
+            addCrossoriginAttrubuteToScripts(htmlPluginData, callback);
+        });
+    });
+};
+
+module.exports = HtmlWebpackCrossoriginPlugin;
